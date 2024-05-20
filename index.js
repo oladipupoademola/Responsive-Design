b14878597a6bd3f91c154192633faa2de59356d5
Ademola <oladipupoademola09@mail.com>
2024-05-20 13:39:00 +0100

    Responsive Design

---
+++ index.js
@@ -0,0 +1,6 @@
+let menu=document.querySelector("#menu-icon")
+let navbar=document.querySelector(".links")
+menu.onclick=()=>{
+    menu.classList.toggle("fa-x")
+    navbar.classList.toggle("open")
+}
